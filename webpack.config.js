b2ae13c64a235ccdfa90eb3575ae670e9de9d414
swanie21 <kswanie21@gmail.com>
2017-03-06 20:51:11 -0700

    add styling

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,8 @@ module.exports = {
       { test: /\.scss$/, loader: 'style!css!resolve-url!sass?sourceMap' },
       { test: /\.png$/, loader: 'url-loader', query: { mimetype: 'image/png'} },
       { test: /\.jpg$/, loader: 'url-loader', query: { mimetype: 'image/jpg'} },
+      { test: /\.(jpg|png|svg)$/, loader: 'url-loader', options: { limit: 25000, },},
+      { test: /\.(jpg|png|svg)$/, loader: 'file-loader', options: { name: '[path][name].[hash].[ext]',},},
       { test: /\.jsx?$/, exclude: /(node_modules|bower_components)/, loader: 'babel',
         query: { presets: ['es2015', 'react'], },
       },
